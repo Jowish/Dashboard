3522bb9db9ba4c44f0c5f00b0259076309f751da
Lolbol <78195864+Jowish@users.noreply.github.com>
2025-04-14 22:45:37 -0300

    reorganize pages

--- src/app/habits/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Habits() {
-    return (
-        <div>
-            <h1>Manage habits</h1>
-        </div>
-    );
-}
---
+++ src/app/manage/page.tsx
@@ -0,0 +1,7 @@
+export default function ManageHabits() {
+    return (
+        <div>
+            <h1>Manage Habits</h1>
+        </div>
+    );
+}
