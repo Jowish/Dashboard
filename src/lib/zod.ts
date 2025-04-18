c61fe80c7b8de3597defe1f385ef1f09cb2a1f39
Lolbol <78195864+Jowish@users.noreply.github.com>
2025-04-18 19:15:19 -0300

    added register schema

--- src/lib/zod.ts
+++ src/lib/zod.ts
@@ -5,6 +5,20 @@ export const loginSchema = object({
         1,
         "Username is required"
     ),
+    password: string({ required_error: "Password is required" }).min(
+        1,
+        "Password is required"
+    ),
+});
+
+export const registerSchema = object({
+    username: string({ required_error: "Username is required" }).min(
+        1,
+        "Username is requried"
+    ),
+    email: string({ required_error: "Email is required" })
+        .min(1, "Email is required")
+        .email("Invalid email"),
     password: string({ required_error: "Password is required" })
         .min(1, "Password is required")
         .min(3, "Password must be more than 3 characters")
