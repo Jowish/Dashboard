69c041e79afe5a9db7c6e6079a60ac54bb961342
Lolbol <78195864+Jowish@users.noreply.github.com>
2025-04-18 17:41:00 -0300

    removed email from login

--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -1,25 +1,19 @@
 export default function LoginForm() {
     return (
-        <form className="flex flex-col space-y-10">
+        <form className="flex flex-col space-y-4">
             <input
-                className="bg-white text-black p-4 rounded-lg w-56"
+                className="bg-white text-black p-2 rounded-lg w-56"
                 type="text"
                 name="username"
                 placeholder="User"
             />
             <input
-                className="bg-white text-black p-4 rounded-lg w-56"
-                type="text"
-                name="email"
-                placeholder="Email"
-            />
-            <input
-                className="bg-white text-black p-4 rounded-lg w-56"
+                className="bg-white text-black p-2 rounded-lg w-56"
                 type="password"
                 name="password"
                 placeholder="Password"
             />
-            <button className="bg-blue-700 text-white py-3 px-6 rounded-lg w-fit">
+            <button className="bg-blue-700 text-white py-2 px-4 rounded-lg w-fit">
                 Login
             </button>
         </form>
