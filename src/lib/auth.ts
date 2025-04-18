b916cd8eb00af8717a0d152747aa9050e0e23b9b
Lolbol <78195864+Jowish@users.noreply.github.com>
2025-04-18 19:32:15 -0300

    added credentials auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,39 @@
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { loginSchema } from "./zod";
+import { db } from "@/db";
+import { users } from "@/db/schema";
+import { sql } from "drizzle-orm";
+import { use } from "react";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-    providers: [],
+    providers: [
+        Credentials({
+            credentials: {
+                username: {},
+                password: {},
+            },
+            authorize: async (credentials) => {
+                let user = null;
+
+                const { username, password } = await credentials;
+
+                const data = await db
+                    .select()
+                    .from(users)
+                    .where(
+                        sql`${users.username} = ${username} and ${users.password} = ${password}`
+                    );
+
+                if (!data || data.length === 0)
+                    throw new Error("Invalid credentials");
+                user = data[0];
+
+                return {
+                    id: user.id.toString(),
+                    name: user.username,
+                };
+            },
+        }),
+    ],
 });
