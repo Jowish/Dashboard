bef44212e001ae5c43528e53889e77a5bdefa918
Lolbol <78195864+Jowish@users.noreply.github.com>
2025-04-18 18:49:35 -0300

    made login components client and dynamic

--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -1,18 +1,30 @@
+"use client";
+import { login } from "@/app/login/actions";
+import { useActionState } from "react";
+
 export default function LoginForm() {
+    const [state, action] = useActionState(login, undefined);
+
     return (
-        <form className="flex flex-col space-y-4">
+        <form className="flex flex-col space-y-4" action={action}>
             <input
                 className="bg-white text-black p-2 rounded-lg w-56"
                 type="text"
                 name="username"
                 placeholder="Username"
             />
+            {state?.errors?.username && (
+                <p className="text-red-500">{state.errors.username}</p>
+            )}
             <input
                 className="bg-white text-black p-2 rounded-lg w-56"
                 type="password"
                 name="password"
                 placeholder="Password"
             />
+            {state?.errors?.password && (
+                <p className="text-red-500">{state.errors.password}</p>
+            )}
             <button className="bg-blue-700 text-white py-2 px-4 rounded-lg w-fit">
                 Login
             </button>
