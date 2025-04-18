15d648be3a13e1e513827e403382df5d2fbfda0b
Lolbol <78195864+Jowish@users.noreply.github.com>
2025-04-18 17:42:00 -0300

    added zod login schema

---
+++ src/lib/zod.ts
@@ -0,0 +1,12 @@
+import { object, string } from "zod";
+
+export const loginInSchema = object({
+    username: string({ required_error: "Username is required" }).min(
+        1,
+        "Username is required"
+    ),
+    password: string({ required_error: "Password is required" })
+        .min(1, "Password is required")
+        .min(3, "Password must be more than 3 characters")
+        .max(42, "Password must be less than 42 characters"),
+});
