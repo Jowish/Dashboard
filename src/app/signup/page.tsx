5bb48158935372326380422f4c5751f70d43695e
Lolbol <78195864+Jowish@users.noreply.github.com>
2025-04-19 17:06:58 -0300

    added redirect to signup page and some spacing

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,15 @@
 import SignupForm from "@/components/signupForm";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function Signup() {
+export default async function Signup() {
+    const session = await auth();
+
+    // if (session?.user) redirect("/dashboard");
+
     return (
-        <div>
-            <h1>Sign Up</h1>
+        <div className="flex flex-col w-screen items-center p-12 space-y-4">
+            <h1 className="text-2xl">Sign Up</h1>
             <SignupForm />
         </div>
     );
