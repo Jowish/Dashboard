18cd0459cae9b3b3cb8b63dc2c21772a94a66c81
Lolbol <78195864+Jowish@users.noreply.github.com>
2025-04-20 17:30:58 -0300

    added redirect to login page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,11 @@
-export default function Dashboard() {
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function Dashboard() {
+    const session = await auth();
+
+    if (!session) redirect("/login");
+
     return (
         <div>
             <h1>Dashboard</h1>
--- src/app/manage/page.tsx
+++ src/app/manage/page.tsx
@@ -1,4 +1,11 @@
-export default function ManageHabits() {
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function ManageHabits() {
+    const session = await auth();
+
+    if (!session) redirect("/login");
+
     return (
         <div>
             <h1>Manage Habits</h1>
