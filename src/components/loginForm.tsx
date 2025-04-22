50ffcbf87706f37e8b2f3edf7a082aaaaa81af91
Lolbol <78195864+Jowish@users.noreply.github.com>
2025-04-22 10:30:02 -0300

    removed unused code

--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { login } from "@/lib/actions";
-import React, { FormEventHandler, useActionState } from "react";
 import { Form, FormControl, FormField, FormItem } from "./ui/form";
 import { Input } from "./ui/input";
 import { useForm } from "react-hook-form";
@@ -9,15 +8,6 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { loginSchema } from "@/lib/zod";
 import { Button } from "./ui/button";
-
-interface FormElements extends HTMLFormControlsCollection {
-    username: HTMLInputElement;
-    password: HTMLInputElement;
-}
-
-interface UserFormElemet extends HTMLFormElement {
-    readonly elements: FormElements;
-}
 
 export default function LoginForm() {
     const form = useForm<z.infer<typeof loginSchema>>({
@@ -71,30 +61,4 @@ export default function LoginForm() {
             </form>
         </Form>
     );
-
-    // return (
-    //     <form className="flex flex-col space-y-4" action={action}>
-    //         <input
-    //             className="bg-white text-black p-2 rounded-lg w-56"
-    //             type="text"
-    //             name="username"
-    //             placeholder="Username"
-    //         />
-    //         {state?.errors?.username && (
-    //             <p className="text-red-500">{state.errors.username}</p>
-    //         )}
-    //         <input
-    //             className="bg-white text-black p-2 rounded-lg w-56"
-    //             type="password"
-    //             name="password"
-    //             placeholder="Password"
-    //         />
-    //         {state?.errors?.password && (
-    //             <p className="text-red-500">{state.errors.password}</p>
-    //         )}
-    //         <button className="bg-blue-700 text-white py-2 px-4 rounded-lg w-fit">
-    //             Login
-    //         </button>
-    //     </form>
-    // );
 }
