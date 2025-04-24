062e9346cc5c0930157f9dc785aea2b4ba5f0d33
Lolbol <78195864+Jowish@users.noreply.github.com>
2025-04-24 15:52:05 -0300

    added tarkSchema

--- src/lib/zod.ts
+++ src/lib/zod.ts
@@ -1,4 +1,4 @@
-import { object, string } from "zod";
+import { array, boolean, object, string } from "zod";
 
 export const loginSchema = object({
     username: string({ required_error: "Username is required" }).min(
@@ -23,4 +23,24 @@ export const registerSchema = object({
         .min(1, "Password is required")
         .min(3, "Password must be more than 3 characters")
         .max(42, "Password must be less than 42 characters"),
+});
+
+export const taskSchema = object({
+    title: string({ required_error: "Title is required" }).min(
+        1,
+        "Title is required"
+    ),
+    description: string({ required_error: "Description is required" }).min(
+        1,
+        "Description is required"
+    ),
+    hour: string({ required_error: "Hour is required" }).min(
+        1,
+        "Hour is required"
+    ),
+    days: array(boolean())
+        .min(1, "At least one day is required")
+        .refine((day) => day.some((day) => day === true), {
+            message: "At least one day is required",
+        }),
 });
